99d19da4d0f4e7d8942ff7588d71388b479cde2c
DamienDeSousa <desousadamien30@gmail.com>
2024-02-11 11:47:31 +0100

    use dompdf

--- api/index.php
+++ api/index.php
@@ -1,4 +1,24 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
-phpinfo();
+use Dompdf\Dompdf;
+
+$json = file_get_contents('php://input');
+$data = json_decode($json, true);
+
+if (!array_key_exists('html', $data)) {
+    echo 'No html provided';
+    exit;
+}
+
+$html = $data['html'];
+
+$dompdf = new Dompdf();
+$dompdf->loadHtml($websiteContent);
+
+// (Optional) Setup the paper size and orientation, 'landscape' or 'portrait'
+$dompdf->setPaper('A4');
+// Render the HTML as PDF
+$dompdf->render();
+// Output the generated PDF to Browser
+$dompdf->stream();
