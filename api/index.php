dc723f7963937ce37720f1f65ad45b8021d44aea
DamienDeSousa <desousadamien30@gmail.com>
2024-02-12 08:19:36 +0100

    try to wait for JS

--- api/index.php
+++ api/index.php
@@ -1,4 +1,7 @@
 <?php
+// set_time_limit(0);
+ini_set('max_execution_time', '300');
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Dompdf\Dompdf;
@@ -18,8 +21,14 @@ if (!array_key_exists('html', $data)) {
 
 $html = $data['html'];
 
-$dompdf = new Dompdf();
-$dompdf->loadHtml($websiteContent);
+// verify if the given html is a valid url
+$regex = "/\b(?:(?:https?|ftp):\/\/|www\.)[-a-zA-Z0-9+&@#\/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#\/%=~_|]/";
+if (filter_var($html, FILTER_VALIDATE_URL) !== false) {
+    $html = file_get_contents($html);
+}
+
+$dompdf = new Dompdf(['enable_remote' => true, 'isJavascriptEnabled' => true]);
+$dompdf->loadHtml($html);
 
 // (Optional) Setup the paper size and orientation, 'landscape' or 'portrait'
 $dompdf->setPaper('A4');
